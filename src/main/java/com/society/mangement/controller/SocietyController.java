c29c92a0eac87fedb2d765ddf76a3e1f7ca59ece
jitgupta <jitendra.gupta1899@gmail.com>
2020-04-12 13:54:37 +0530

    Committed for spring boot admin

--- src/main/java/com/society/mangement/controller/SocietyController.java
+++ src/main/java/com/society/mangement/controller/SocietyController.java
@@ -7,12 +7,14 @@ import javax.validation.Valid;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.society.mangement.bean.RegisterSocietyUser;
@@ -64,6 +66,5 @@ public class SocietyController {
 
 	    return registerUserRepository.save(registerUsderbySociety);
 
-	}	
-
+	}		
 }
